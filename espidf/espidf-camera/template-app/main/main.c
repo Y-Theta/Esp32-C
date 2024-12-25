4505c3aa737366bef2c4c0f947489e74c3fbed1f
杨 杰铭 <yangjm@mctech.vip>
2024-12-25 18:03:38 +0800

    add esp32 camera about

---
+++ espidf/espidf-camera/template-app/main/main.c
@@ -0,0 +1,21 @@
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_http_client.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "esp_mac.h"
+#include "camera/hal_config.h"
+#include "sdkconfig.h"
+
+#include <inttypes.h>
+#include <nvs_flash.h>
+#include <stdio.h>
+#include <esp_camera.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void)
+{
+
+}
