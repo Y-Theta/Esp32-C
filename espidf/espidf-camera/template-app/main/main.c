66268d0b835ef82fd734c80519ebb82020efce5f
杨 杰铭 <yangjm@mctech.vip>
2024-12-25 18:43:27 +0800

    add config

--- espidf/espidf-camera/template-app/main/main.c
+++ espidf/espidf-camera/template-app/main/main.c
@@ -6,6 +6,7 @@
 #include "esp_mac.h"
 #include "camera/hal_config.h"
 #include "sdkconfig.h"
+#include "esp_http_client.h"
 
 #include <inttypes.h>
 #include <nvs_flash.h>
@@ -15,7 +16,409 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#define TAG "PY260 Transfer"
+#define WIFI_AUTHMODE WIFI_AUTH_WPA2_PSK
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+#define WIFI_SSID "s20154530"
+#define WIFI_PASSWORD "Y20154530"
+
+#define MAX_HTTP_RECV_BUFFER 512
+#define MAX_HTTP_OUTPUT_BUFFER 2048
+
+static const int WIFI_RETRY_ATTEMPT = 3;
+static int wifi_retry_count = 0;
+
+static esp_netif_t *tutorial_netif = NULL;
+static esp_event_handler_instance_t ip_event_handler;
+static esp_event_handler_instance_t wifi_event_handler;
+static EventGroupHandle_t s_wifi_event_group = NULL;
+
+void InitCamera();
+esp_err_t InitNetwork();
+esp_err_t ConnectNetwork();
+void http_rest_with_url();
+
+static void ip_event_cb(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    ESP_LOGI(TAG, "Handling IP event, event code 0x%" PRIx32, event_id);
+    switch (event_id) {
+    case (IP_EVENT_STA_GOT_IP):
+        ip_event_got_ip_t *event_ip = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "Got IP: " IPSTR, IP2STR(&event_ip->ip_info.ip));
+        wifi_retry_count = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+        break;
+    case (IP_EVENT_STA_LOST_IP):
+        ESP_LOGI(TAG, "Lost IP");
+        break;
+    case (IP_EVENT_GOT_IP6):
+        ip_event_got_ip6_t *event_ip6 = (ip_event_got_ip6_t *)event_data;
+        ESP_LOGI(TAG, "Got IPv6: " IPV6STR, IPV62STR(event_ip6->ip6_info.ip));
+        wifi_retry_count = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+        break;
+    default:
+        ESP_LOGI(TAG, "IP event not handled");
+        break;
+    }
+}
+
+static void wifi_event_cb(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    ESP_LOGI(TAG, "Handling Wi-Fi event, event code 0x%" PRIx32, event_id);
+
+    switch (event_id) {
+    case (WIFI_EVENT_WIFI_READY):
+        ESP_LOGI(TAG, "Wi-Fi ready");
+        break;
+    case (WIFI_EVENT_SCAN_DONE):
+        ESP_LOGI(TAG, "Wi-Fi scan done");
+        break;
+    case (WIFI_EVENT_STA_START):
+        ESP_LOGI(TAG, "Wi-Fi started, connecting to AP...");
+        esp_wifi_connect();
+        break;
+    case (WIFI_EVENT_STA_STOP):
+        ESP_LOGI(TAG, "Wi-Fi stopped");
+        break;
+    case (WIFI_EVENT_STA_CONNECTED):
+        ESP_LOGI(TAG, "Wi-Fi connected");
+        break;
+    case (WIFI_EVENT_STA_DISCONNECTED):
+        ESP_LOGI(TAG, "Wi-Fi disconnected");
+        if (wifi_retry_count < WIFI_RETRY_ATTEMPT) {
+            ESP_LOGI(TAG, "Retrying to connect to Wi-Fi network...");
+            esp_wifi_connect();
+            wifi_retry_count++;
+        } else {
+            ESP_LOGI(TAG, "Failed to connect to Wi-Fi network");
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        break;
+    case (WIFI_EVENT_STA_AUTHMODE_CHANGE):
+        ESP_LOGI(TAG, "Wi-Fi authmode changed");
+        break;
+    default:
+        ESP_LOGI(TAG, "Wi-Fi event not handled");
+        break;
+    }
+}
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+            /*
+             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+             *  However, event handler can also be used in case chunked encoding is used.
+             */
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // If user_data buffer is configured, copy the response into the buffer
+                if (evt->user_data) {
+                    memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+                } else {
+                    if (output_buffer == NULL) {
+                        output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    memcpy(output_buffer + output_len, evt->data, evt->data_len);
+                }
+                output_len += evt->data_len;
+            }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            output_len = 0;
+            break;
+        case HTTP_EVENT_REDIRECT:
+            ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
+            esp_http_client_set_header(evt->client, "From", "user@example.com");
+            esp_http_client_set_header(evt->client, "Accept", "text/html");
+            break;
+    }
+    return ESP_OK;
+}
+
 void app_main(void)
 {
+  vTaskDelay(3000 / portTICK_PERIOD_MS);
 
+    printf("Program Started!\n");
+    printf("Init WIFI !\n");
+    ESP_ERROR_CHECK(InitNetwork());
+    if (ConnectNetwork(WIFI_SSID, WIFI_PASSWORD) != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to connect to Wi-Fi network");
+    }
+
+    wifi_ap_record_t ap_info;
+    esp_err_t ret = esp_wifi_sta_get_ap_info(&ap_info);
+    if (ret == ESP_ERR_WIFI_CONN) {
+        ESP_LOGE(TAG, "Wi-Fi station interface not initialized");
+    }
+    else if (ret == ESP_ERR_WIFI_NOT_CONNECT) {
+        ESP_LOGE(TAG, "Wi-Fi station is not connected");
+    } else {
+        ESP_LOGI(TAG, "--- Access Point Information ---");
+        ESP_LOG_BUFFER_HEX("MAC Address", ap_info.bssid, sizeof(ap_info.bssid));
+        ESP_LOG_BUFFER_CHAR("SSID", ap_info.ssid, sizeof(ap_info.ssid));
+        ESP_LOGI(TAG, "Primary Channel: %d", ap_info.primary);
+        ESP_LOGI(TAG, "RSSI: %d", ap_info.rssi);
+    }
+    http_rest_with_url();
+    printf("Init Camera !\n");
+    // InitCamera();
+    fflush(stdout);
+    // esp_restart();
+    while (true)
+    {
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
+}
+
+esp_err_t InitNetwork() {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    s_wifi_event_group = xEventGroupCreate();
+
+    ret = esp_netif_init();
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize TCP/IP network stack");
+        return ret;
+    }
+
+    ret = esp_event_loop_create_default();
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to create default event loop");
+        return ret;
+    }
+
+    ret = esp_wifi_set_default_wifi_sta_handlers();
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to set default handlers");
+        return ret;
+    }
+
+    tutorial_netif = esp_netif_create_default_wifi_sta();
+    if (tutorial_netif == NULL) {
+        ESP_LOGE(TAG, "Failed to create default WiFi STA interface");
+        return ESP_FAIL;
+    }
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_cb,
+                                                        NULL,
+                                                        &wifi_event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &ip_event_cb,
+                                                        NULL,
+                                                        &ip_event_handler));
+    return ret;
+}
+
+esp_err_t ConnectNetwork(char *wifi_ssid, char *wifi_password) {
+    wifi_config_t wifi_config = {
+        .sta = {
+            // this sets the weakest authmode accepted in fast scan mode (default)
+            .threshold.authmode = WIFI_AUTHMODE,
+        },
+    };
+
+    strncpy((char *)wifi_config.sta.ssid, wifi_ssid, sizeof(wifi_config.sta.ssid));
+    strncpy((char *)wifi_config.sta.password, wifi_password, sizeof(wifi_config.sta.password));
+
+    ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));          // default is WIFI_PS_MIN_MODEM
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM)); // default is WIFI_STORAGE_FLASH
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+
+    ESP_LOGI(TAG, "Connecting to Wi-Fi network: %s", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE, pdFALSE, portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "Connected to Wi-Fi network: %s", wifi_config.sta.ssid);
+        return ESP_OK;
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGE(TAG, "Failed to connect to Wi-Fi network: %s", wifi_config.sta.ssid);
+        return ESP_FAIL;
+    }
+
+    ESP_LOGE(TAG, "Unexpected Wi-Fi error");
+    return ESP_FAIL;
+}
+
+void http_rest_with_url(void)
+{
+    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+    /**
+     * NOTE: All the configuration parameters for http_client must be spefied either in URL or as host and path parameters.
+     * If host and path parameters are not set, query parameter will be ignored. In such cases,
+     * query parameter should be specified in URL.
+     *
+     * If URL as well as host and path parameters are specified, values of host and path will be considered.
+     */
+    esp_http_client_config_t config = {
+        .host = "http://38.147.174.195:20678/firefly",
+        .path = "/get",
+        .query = "esp",
+        .event_handler = _http_event_handler,
+        .user_data = local_response_buffer,        // Pass address of local buffer to get response
+        
+        .disable_auto_redirect = true,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    // GET
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %lld",
+                esp_http_client_get_status_code(client),
+                esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+    ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+
+    // // POST
+    // const char *post_data = "{\"field1\":\"value1\"}";
+    // esp_http_client_set_url(client, "http://httpbin.org/post");
+    // esp_http_client_set_method(client, HTTP_METHOD_POST);
+    // esp_http_client_set_header(client, "Content-Type", "application/json");
+    // esp_http_client_set_post_field(client, post_data, strlen(post_data));
+    // err = esp_http_client_perform(client);
+    // if (err == ESP_OK) {
+    //     ESP_LOGI(TAG, "HTTP POST Status = %d, content_length = %lld",
+    //             esp_http_client_get_status_code(client),
+    //             esp_http_client_get_content_length(client));
+    // } else {
+    //     ESP_LOGE(TAG, "HTTP POST request failed: %s", esp_err_to_name(err));
+    // }
+
+    // //PUT
+    // esp_http_client_set_url(client, "http://httpbin.org/put");
+    // esp_http_client_set_method(client, HTTP_METHOD_PUT);
+    // err = esp_http_client_perform(client);
+    // if (err == ESP_OK) {
+    //     ESP_LOGI(TAG, "HTTP PUT Status = %d, content_length = %lld",
+    //             esp_http_client_get_status_code(client),
+    //             esp_http_client_get_content_length(client));
+    // } else {
+    //     ESP_LOGE(TAG, "HTTP PUT request failed: %s", esp_err_to_name(err));
+    // }
+
+    // //PATCH
+    // esp_http_client_set_url(client, "http://httpbin.org/patch");
+    // esp_http_client_set_method(client, HTTP_METHOD_PATCH);
+    // esp_http_client_set_post_field(client, NULL, 0);
+    // err = esp_http_client_perform(client);
+    // if (err == ESP_OK) {
+    //     ESP_LOGI(TAG, "HTTP PATCH Status = %d, content_length = %lld",
+    //             esp_http_client_get_status_code(client),
+    //             esp_http_client_get_content_length(client));
+    // } else {
+    //     ESP_LOGE(TAG, "HTTP PATCH request failed: %s", esp_err_to_name(err));
+    // }
+
+    // //DELETE
+    // esp_http_client_set_url(client, "http://httpbin.org/delete");
+    // esp_http_client_set_method(client, HTTP_METHOD_DELETE);
+    // err = esp_http_client_perform(client);
+    // if (err == ESP_OK) {
+    //     ESP_LOGI(TAG, "HTTP DELETE Status = %d, content_length = %lld",
+    //             esp_http_client_get_status_code(client),
+    //             esp_http_client_get_content_length(client));
+    // } else {
+    //     ESP_LOGE(TAG, "HTTP DELETE request failed: %s", esp_err_to_name(err));
+    // }
+
+    // //HEAD
+    // esp_http_client_set_url(client, "http://httpbin.org/get");
+    // esp_http_client_set_method(client, HTTP_METHOD_HEAD);
+    // err = esp_http_client_perform(client);
+    // if (err == ESP_OK) {
+    //     ESP_LOGI(TAG, "HTTP HEAD Status = %d, content_length = %lld",
+    //             esp_http_client_get_status_code(client),
+    //             esp_http_client_get_content_length(client));
+    // } else {
+    //     ESP_LOGE(TAG, "HTTP HEAD request failed: %s", esp_err_to_name(err));
+    // }
+
+    esp_http_client_cleanup(client);
+}
+
+
+void InitCamera() {
+    camera_config_t config;
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = CAMERA_PIN_D0;
+    config.pin_d1 = CAMERA_PIN_D1;
+    config.pin_d2 = CAMERA_PIN_D2;
+    config.pin_d3 = CAMERA_PIN_D3;
+    config.pin_d4 = CAMERA_PIN_D4;
+    config.pin_d5 = CAMERA_PIN_D5;
+    config.pin_d6 = CAMERA_PIN_D6;
+    config.pin_d7 = CAMERA_PIN_D7;
+    config.pin_xclk = CAMERA_PIN_XCLK;
+    config.pin_pclk = CAMERA_PIN_PCLK;
+    config.pin_vsync = CAMERA_PIN_VSYNC;
+    config.pin_href = CAMERA_PIN_HREF;
+    config.pin_sccb_sda = CAMERA_PIN_SIOD;
+    config.pin_sccb_scl = CAMERA_PIN_SIOC;
+    config.pin_pwdn = CAMERA_PIN_PWDN;
+    config.pin_reset = CAMERA_PIN_RESET;
+    config.xclk_freq_hz = XCLK_FREQ_HZ;
+    config.pixel_format = PIXFORMAT_JPEG;
+    config.frame_size = FRAMESIZE_VGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK) {
+        printf("camera init failed\n");
+    }
 }
